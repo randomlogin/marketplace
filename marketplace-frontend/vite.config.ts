8f3b02f242d01f5964e12c210020fd97db281305
Alexander Shevtsov <randomlogin76@gmail.com>
2025-02-13 20:02:11 +0100

    upd allowed hosts

--- marketplace-frontend/vite.config.ts
+++ marketplace-frontend/vite.config.ts
@@ -3,19 +3,35 @@ import preact from '@preact/preset-vite';
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8123',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
+	plugins: [preact(), tailwindcss()],
+	resolve: {
+		alias: {
+			'@': '/src',
+		},
+	},
+	preview: {
+		host: true,
+		port: 3000,
+		allowedHosts: [
+			'spacesprotocol-marketplace-1981273617.us-east-1.elb.amazonaws.com',
+			'spaces.market',
+			'testnet.spaces.market'
+		]
+	},
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://localhost:8123',
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		},
+		host: true,
+		port: 3000,
+		allowedHosts: [
+			'spacesprotocol-marketplace-1981273617.us-east-1.elb.amazonaws.com',
+			'spaces.market',
+			'testnet.spaces.market'
+		]
+	},
 });
